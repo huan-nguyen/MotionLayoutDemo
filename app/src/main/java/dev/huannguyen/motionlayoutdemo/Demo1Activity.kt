ba12c48505ad5123a4b898ec1f6930664dcff4a6
Huan Nguyen <huan.nguyen@rea-group.com>
2020-10-19 15:28:25 +1100

    Add basic animation demo

---
+++ app/src/main/java/dev/huannguyen/motionlayoutdemo/Demo1Activity.kt
@@ -0,0 +1,11 @@
+package dev.huannguyen.motionlayoutdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Demo1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo1)
+    }
+}
---
+++ app/src/main/java/dev/huannguyen/motionlayoutdemo/MainActivity.kt
@@ -0,0 +1,31 @@
+package dev.huannguyen.motionlayoutdemo
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.TypedValue
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.setPadding
+import kotlinx.android.synthetic.main.activity_main.container
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        addDemo<Demo1Activity>("Basic Animation")
+    }
+
+    private inline fun <reified T: Activity> addDemo(name: String) {
+        val view = TextView(this).apply {
+            text = name
+            setPadding(32)
+            setOnClickListener { startActivity(Intent(this@MainActivity, T::class.java)) }
+            val typedValue = TypedValue()
+            context.theme.resolveAttribute(android.R.attr.selectableItemBackground, typedValue, true)
+            setBackgroundResource(typedValue.resourceId)
+        }
+        container.addView(view)
+    }
+}
