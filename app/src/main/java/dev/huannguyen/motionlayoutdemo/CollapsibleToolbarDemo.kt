5710a4a31792c0696d0bbb2d59487b748d05d797
Huan Nguyen <huan.nguyentuong@gmail.com>
2020-10-26 14:01:40 +1100

    Add second demo for swipeable view holder

--- app/src/main/java/dev/huannguyen/motionlayoutdemo/CollapsibleToolbarDemo.kt
+++ app/src/main/java/dev/huannguyen/motionlayoutdemo/CollapsibleToolbarDemo.kt
@@ -2,9 +2,13 @@ package dev.huannguyen.motionlayoutdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import dev.huannguyen.motionlayoutdemo.databinding.CollapsibleToolbarDemoActivityBinding
+import dev.huannguyen.motionlayoutdemo.databinding.ItemViewBinding
 
 class CollapsibleToolbarDemo : AppCompatActivity() {
     private lateinit var binding: CollapsibleToolbarDemoActivityBinding
@@ -18,5 +22,28 @@ class CollapsibleToolbarDemo : AppCompatActivity() {
             adapter = Adapter(testData)
             addItemDecoration(DividerItemDecoration(this@CollapsibleToolbarDemo, RecyclerView.VERTICAL))
         }
+    }
+}
+
+val testData = (1..20).map { "Item $it" }
+
+class Adapter(private val items: List<String>) : RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+}
+
+class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private val binding = ItemViewBinding.bind(view)
+
+    fun bind(value: String) {
+        binding.name.text = value
     }
 }
--- app/src/main/java/dev/huannguyen/motionlayoutdemo/ViewHolderSwipeDemo.kt
+++ app/src/main/java/dev/huannguyen/motionlayoutdemo/ViewHolderSwipeDemo.kt
@@ -1,27 +1,11 @@
 package dev.huannguyen.motionlayoutdemo
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.RecyclerView
-import dev.huannguyen.motionlayoutdemo.databinding.ItemViewBinding
-import dev.huannguyen.motionlayoutdemo.databinding.ViewHolderSwipeDemoActivityBinding
 
 class ViewHolderSwipeDemo : AppCompatActivity() {
-    private lateinit var binding: ViewHolderSwipeDemoActivityBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ViewHolderSwipeDemoActivityBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.recyclerView.run {
-            adapter = Adapter(testData)
-            addItemDecoration(DividerItemDecoration(this@ViewHolderSwipeDemo, RecyclerView.VERTICAL))
-        }
+        setContentView(R.layout.view_holder_swipe_demo_activity)
     }
 }
